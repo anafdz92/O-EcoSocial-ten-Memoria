72ef25e4a81d0ef13f557c63a3574b18ed82f032
Ana Fernandez <anafdz92@gmail.com>
2020-09-15 12:21:33 +0200

    add line and title

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Conflicts from "../data/Conflicts";
 const App = () => {
   return (
     <div className="mainList">
+      <h1 className="title"> O EcoSocial ten Memoria </h1>
       <Line />
       <Events />
     </div>
