9fcaa49606375330a165b4ffe434407d16f151c7
Ana Fernandez <anafdz92@gmail.com>
2020-09-24 18:53:18 +0200

    add gitignore

---
+++ docs/precache-manifest.e58864f59937836b787fb260cc0bae65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b058f12c65dcede7dfc22883262e4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0393875c671f22589c5a",
+    "url": "./static/css/main.9c58d45b.chunk.css"
+  },
+  {
+    "revision": "cabcd58b1ed282c2e1c8",
+    "url": "./static/js/2.b7e4377c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b7e4377c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0393875c671f22589c5a",
+    "url": "./static/js/main.3e43edba.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  },
+  {
+    "revision": "001db304e99a169062478c90152157ef",
+    "url": "./static/media/img.001db304.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2ea30aceeb4ca039cf6e47916f30776.js"
+  "./precache-manifest.e58864f59937836b787fb260cc0bae65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
