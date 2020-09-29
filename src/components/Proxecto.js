3d271d40416b0a9cd0fabcd45bfce41cc8fdc43b
Ana Fernandez <anafdz92@gmail.com>
2020-09-29 16:09:23 +0200

    add

--- src/components/Proxecto.js
+++ src/components/Proxecto.js
@@ -7,12 +7,10 @@ const Proxecto = (props) => {
       <div className="projectContainer">
         <h3> O Proxecto</h3>
         <p>Este proxecto é unha táctica de resistencia fronte a desmemoria.</p>
-        Un arquivo vivo en torno o que reflectir acerca dos conflictos
+        Un arquivo vivo en torno o que reflectir arredor dos conflictos
         ecolóxicos e sociais que atravesan a nosa história contemporánea.
         <p>
-          <p>
-            Unha ollada o pasado recente para confrontar as loitas do presente.
-          </p>
+          <p>Unha ollada o pasado para confrontar as loitas do presente.</p>
         </p>
       </div>
     </div>
